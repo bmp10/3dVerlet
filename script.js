c2d4ab135ed7fd8ff4f3f7647e9c7486f2718f78
bmp10 <89558241+bmp10@users.noreply.github.com>
2022-06-07 11:57:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ const bg = document.getElementById("bg")
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer({
     canvas: bg,
 });
 
 renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(1000, 1000);
 camera.position.setZ(30);
 
 renderer.render(scene, camera)
